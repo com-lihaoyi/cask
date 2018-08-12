9bf8c31fa9321558d7d02f6a5b687cd55a924e7f
Li Haoyi <haoyi.sg@gmail.com>
2018-08-12 21:04:47 +0800

    0.0.1

---
+++ ci/release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eux
+
+echo $GPG_PRIVATE_KEY_B64 | base64 --decode > gpg_key
+
+gpg --import gpg_key
+
+rm gpg_key
+
+mill mill.scalalib.PublishModule/publishAll \
+    lihaoyi:$SONATYPE_PASSWORD \
+    $GPG_PASSWORD \
+    __.publishArtifacts \
+    --release \
+    true
+
+
+mill uploadToGithub $GITHUB_ACCESS_TOKEN
---
+++ ci/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eux
+
+mill -i __.test.test
