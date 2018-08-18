907695586cf144a63b59c26f76bad53907a7fb38
Li Haoyi <haoyi.sg@gmail.com>
2018-08-18 12:19:51 +0800

    Swap out `WebsocketParam` for identical `QueryParamReader`

--- cask/src/cask/endpoints/WebSocketEndpoint.scala
+++ cask/src/cask/endpoints/WebSocketEndpoint.scala
@@ -2,32 +2,7 @@ package cask.endpoints
 
 import cask.internal.Router
 import cask.model.Request
-import io.undertow.server.HttpServerExchange
 import io.undertow.websockets.WebSocketConnectionCallback
-trait WebsocketParam[T] extends Router.ArgReader[Seq[String], T, cask.model.Request]
-
-object WebsocketParam{
-  class NilParam[T](f: (Request, String) => T) extends WebsocketParam[T]{
-    def arity = 0
-    def read(ctx: Request, label: String, v: Seq[String]): T = f(ctx, label)
-  }
-  implicit object HttpExchangeParam extends NilParam[HttpServerExchange](
-    (ctx, label) => ctx.exchange
-  )
-  class SimpleParam[T](f: String => T) extends WebsocketParam[T]{
-    def arity = 1
-    def read(ctx: cask.model.Request, label: String, v: Seq[String]): T = f(v.head)
-  }
-
-  implicit object StringParam extends SimpleParam[String](x => x)
-  implicit object BooleanParam extends SimpleParam[Boolean](_.toBoolean)
-  implicit object ByteParam extends SimpleParam[Byte](_.toByte)
-  implicit object ShortParam extends SimpleParam[Short](_.toShort)
-  implicit object IntParam extends SimpleParam[Int](_.toInt)
-  implicit object LongParam extends SimpleParam[Long](_.toLong)
-  implicit object DoubleParam extends SimpleParam[Double](_.toDouble)
-  implicit object FloatParam extends SimpleParam[Float](_.toFloat)
-}
 
 sealed trait WebsocketResult
 object WebsocketResult{
@@ -39,7 +14,7 @@ class websocket(val path: String, override val subpath: Boolean = false) extends
   type Output = WebsocketResult
   val methods = Seq("websocket")
   type Input = Seq[String]
-  type InputParser[T] = WebsocketParam[T]
+  type InputParser[T] = QueryParamReader[T]
   type Returned = Router.Result[WebsocketResult]
   def wrapFunction(ctx: Request, delegate: Delegate): Returned = delegate(Map())
 
