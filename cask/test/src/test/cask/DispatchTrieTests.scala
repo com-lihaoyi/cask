com-lihaoyi/cask

commit 0e7ee84ecb93c173a6d0bce3eb98d57117066cd7

Rework routeDispatchTries + endpoint lookup for more intuitive 404/405 responses. (#52)
Prevent cask from responding with 405 for an undefined route.
Fixes #51.

Prior to this commit, `prepareRouteTries` created a mapping from
method-name to DispatchTrie (Map[String, DispatchTrie[…]]).

This commit instead creates a DispatchTrie[Map[String, …]],
basically an inversion of the previous result.

The updated tests in minimalApplication and minimalApplication2
have been updated to cover the differences.

cask/src/cask/main/Main.scala
@@ -44,10 +44,10 @@ abstract class Main{
 
   implicit def log: cask.util.Logger = new cask.util.Logger.Console()
 
-  def routeTries = Main.prepareRouteTries(allRoutes)
+  def dispatchTrie = Main.prepareDispatchTrie(allRoutes)
 
   def defaultHandler = new BlockingHandler(
-    new Main.DefaultHandler(routeTries, mainDecorators, debugMode, handleNotFound, handleMethodNotAllowed, handleEndpointError)
+    new Main.DefaultHandler(dispatchTrie, mainDecorators, debugMode, handleNotFound, handleMethodNotAllowed, handleEndpointError)
   )
 
   def handleNotFound() = Main.defaultHandleNotFound()
@@ -72,7 +72,7 @@ abstract class Main{
 }
 
 object Main{
-  class DefaultHandler(routeTries: Map[String, DispatchTrie[(Routes, EndpointMetadata[_])]],
+  class DefaultHandler(dispatchTrie: DispatchTrie[Map[String, (Routes, EndpointMetadata[_])]],
                        mainDecorators: Seq[Decorator[_, _, _]],
                        debugMode: Boolean,
                        handleNotFound: () => Response.Raw,
@@ -99,35 +99,30 @@ object Main{
         (r: Any) => Main.writeResponse(exchange, r.asInstanceOf[Response.Raw])
       )
 
-      val dispatchTrie: DispatchTrie[(Routes, EndpointMetadata[_])] = routeTries.get(effectiveMethod) match {
-        case None =>
-          Main.writeResponse(exchange, handleMethodNotAllowed())
-          return
-        case Some(trie) => trie
-      }
-
       dispatchTrie.lookup(Util.splitPath(exchange.getRequestPath).toList, Map()) match {
         case None => Main.writeResponse(exchange, handleNotFound())
-        case Some(((routes, metadata), routeBindings, remaining)) =>
-          Decorator.invoke(
-            Request(exchange, remaining),
-            metadata.endpoint,
-            metadata.entryPoint.asInstanceOf[EntryPoint[Routes, _]],
-            routes,
-            routeBindings,
-            (mainDecorators ++ routes.decorators ++ metadata.decorators).toList,
-            Nil
-          ) match{
-            case Result.Success(res) => runner(res)
-            case e: Result.Error =>
-              Main.writeResponse(
-                exchange,
-                handleError(routes, metadata, e)
-              )
-              None
+        case Some((methodMap, routeBindings, remaining)) =>
+          methodMap.get(effectiveMethod) match {
+            case None => Main.writeResponse(exchange, handleMethodNotAllowed())
+            case Some((routes, metadata)) =>
+              Decorator.invoke(
+                Request(exchange, remaining),
+                metadata.endpoint,
+                metadata.entryPoint.asInstanceOf[EntryPoint[Routes, _]],
+                routes,
+                routeBindings,
+                (mainDecorators ++ routes.decorators ++ metadata.decorators).toList,
+                Nil
+              ) match {
+                case Result.Success(res) => runner(res)
+                case e: Result.Error =>
+                  Main.writeResponse(
+                    exchange,
+                    handleError(routes, metadata, e)
+                  )
+              }
           }
       }
-      //        println("Completed Request: " + exchange.getRequestPath)
     }catch{case e: Throwable =>
       e.printStackTrace()
     }
@@ -147,23 +142,25 @@ object Main{
     )
   }
 
-  def prepareRouteTries(allRoutes: Seq[Routes]): Map[String, DispatchTrie[(Routes, EndpointMetadata[_])]] = {
-    val routeList = for{
+  def prepareDispatchTrie(allRoutes: Seq[Routes]): DispatchTrie[Map[String, (Routes, EndpointMetadata[_])]] = {
+    val flattenedRoutes = for {
       routes <- allRoutes
-      route <- routes.caskMetadata.value.map(x => x: EndpointMetadata[_])
-    } yield (routes, route)
+      metadata <- routes.caskMetadata.value
+    } yield {
+      val segments = Util.splitPath(metadata.endpoint.path)
+      val methodMap = metadata.endpoint.methods.map(_ -> (routes, metadata: EndpointMetadata[_])).toMap
+      (segments, methodMap, metadata.endpoint.subpath)
+    }
 
-    val allMethods: Set[String] =
-      routeList.flatMap(_._2.endpoint.methods).map(_.toLowerCase).toSet
+    val dispatchInputs = flattenedRoutes.groupBy(_._1).map { case (segments, values) =>
+      val methodMap = values.map(_._2).flatten.toMap
+      val hasSubpath = values.map(_._3).contains(true)
+      (segments, methodMap, hasSubpath)
+    }.toSeq
 
-    allMethods
-      .map { method =>
-        method -> DispatchTrie.construct[(Routes, EndpointMetadata[_])](0,
-          for ((route, metadata) <- routeList if metadata.endpoint.methods.contains(method))
-            yield (Util.splitPath(metadata.endpoint.path): collection.IndexedSeq[String], (route, metadata), metadata.endpoint.subpath)
-        )
-      }.toMap
+    DispatchTrie.construct(0, dispatchInputs)
   }
+
   def writeResponse(exchange: HttpServerExchange, response: Response.Raw) = {
     response.data.headers.foreach{case (k, v) =>
       exchange.getResponseHeaders.put(new HttpString(k), v)

example/minimalApplication/app/test/src/ExampleTests.scala
@@ -27,7 +27,7 @@ object ExampleTests extends TestSuite{
 
       requests.post(s"$host/do-thing", data = "hello").text() ==> "olleh"
 
-      requests.get(s"$host/do-thing", check = false).statusCode ==> 404
+      requests.delete(s"$host/do-thing", check = false).statusCode ==> 405
     }
   }
 }

example/minimalApplication2/app/test/src/ExampleTests.scala
@@ -27,7 +27,7 @@ object ExampleTests extends TestSuite{
 
       requests.post(s"$host/do-thing", data = "hello").text() ==> "olleh"
 
-      requests.get(s"$host/do-thing", check = false).statusCode ==> 404
+      requests.delete(s"$host/do-thing", check = false).statusCode ==> 405
     }
   }
 }

.gitignore
@@ -1,3 +1,6 @@
+.bloop/
+.metals/
+.vscode/
 target/
 *.iml
 .idea

.mill-version
@@ -1,0 +1,2 @@
+0.9.7
+

build.sc
@@ -62,7 +62,7 @@ class CaskMainModule(val crossScalaVersion: String) extends CaskModule {
   def scalacPluginIvyDeps = T{ if (!isDotty) Agg(ivy"com.lihaoyi::acyclic:0.2.0") else Agg() }
 
   object test extends Tests{
-    def testFrameworks = Seq("utest.runner.Framework")
+    def testFramework = "utest.runner.Framework"
     def ivyDeps = Agg(
       ivy"com.lihaoyi::utest::0.7.10",
       ivy"com.lihaoyi::requests::0.6.9"

example/compress/build.sc
@@ -7,7 +7,7 @@ trait AppModule extends CrossScalaModule{
   def ivyDeps = Agg[Dep](
   )
   object test extends Tests{
-    def testFrameworks = Seq("utest.runner.Framework")
+    def testFramework = "utest.runner.Framework"
 
     def ivyDeps = Agg(
       ivy"com.lihaoyi::utest::0.7.10",

example/compress2/build.sc
@@ -6,7 +6,7 @@ trait AppModule extends CrossScalaModule{
   def ivyDeps = Agg[Dep](
   )
   object test extends Tests{
-    def testFrameworks = Seq("utest.runner.Framework")
+    def testFramework = "utest.runner.Framework"
 
     def ivyDeps = Agg(
       ivy"com.lihaoyi::utest::0.7.10",

commit e8184c9cc7fc2706618cf64de393276865747972

Fix DispatchTrie validation and reporting of invalid routes (#70)
This regressed in https://github.com/com-lihaoyi/cask/pull/52, resulting in both false positives (where a `GET` and a `POST` shared the same route, giving an unnecessary error) and false negatives (where multiple `GET`s sharing the same route failed to create an error). The basic problem was that since combining the various HTTP methods into a single routing trie, the old logic comparing uniqueness/duplication/etc. was no longer correct in the new combined trie.

This PR fixes it by doing a `groupBy` to split up the entries in the combined trie by HTTP method, before running essentially the same validation.

We augment the test suite, tightening up cask/test/src/test/cask/DispatchTrieTests.scala to make it stricter, checking exact error messages to ensure we get not just any failure but the *correct* failure when the validation code triggers. This should hopefully catch this sort of regression in future.

cask/src/cask/internal/DispatchTrie.scala
@@ -1,8 +1,9 @@
 package cask.internal
 import collection.mutable
 object DispatchTrie{
-  def construct[T](index: Int,
-                   inputs: collection.Seq[(collection.IndexedSeq[String], T, Boolean)]): DispatchTrie[T] = {
+  def construct[T, V](index: Int,
+                      inputs: collection.Seq[(collection.IndexedSeq[String], T, Boolean)])
+                     (validationGroups: T => Seq[V]): DispatchTrie[T] = {
     val continuations = mutable.Map.empty[String, mutable.Buffer[(collection.IndexedSeq[String], T, Boolean)]]
 
     val terminals = mutable.Buffer.empty[(collection.IndexedSeq[String], T, Boolean)]
@@ -17,29 +18,67 @@ object DispatchTrie{
       }
     }
 
+    for(group <- inputs.flatMap(t => validationGroups(t._2)).distinct) {
+      val groupTerminals = terminals.flatMap{case (path, v, allowSubpath) =>
+        validationGroups(v)
+          .filter(_ == group)
+          .map{group => (path, v, allowSubpath, group)}
+      }
+
+      val groupContinuations = continuations
+        .map { case (k, vs) =>
+          k -> vs.flatMap { case (path, v, allowSubpath) =>
+            validationGroups(v)
+              .filter(_ == group)
+              .map { group => (path, v, allowSubpath, group) }
+          }
+        }
+        .filter(_._2.nonEmpty)
+
+      validateGroup(groupTerminals, groupContinuations)
+    }
+
+    DispatchTrie[T](
+      current = terminals.headOption.map(x => x._2 -> x._3),
+      children = continuations
+        .map{ case (k, vs) => (k, construct(index + 1, vs)(validationGroups))}
+        .toMap
+    )
+  }
+
+  def validateGroup[T, V](terminals: collection.Seq[(collection.Seq[String], T, Boolean, V)],
+                          continuations: mutable.Map[String, mutable.Buffer[(collection.IndexedSeq[String], T, Boolean, V)]]) = {
     val wildcards = continuations.filter(_._1(0) == ':')
-    if (terminals.length > 1){
+
+    def renderTerminals = terminals
+      .map{case (path, v, allowSubpath, group) => s"$group${renderPath(path)}"}
+      .mkString(", ")
+
+    def renderContinuations = continuations.toSeq
+        .flatMap(_._2)
+        .map{case (path, v, allowSubpath, group) => s"$group${renderPath(path)}"}
+        .mkString(", ")
+
+    if (terminals.length > 1) {
       throw new Exception(
-        "More than one endpoint has the same path: " +
-          terminals.map(_._1.map(_.mkString("/"))).mkString(", ")
+        s"More than one endpoint has the same path: $renderTerminals"
       )
-    } else if(wildcards.size >= 1 && continuations.size > 1) {
+    }
+
+    if (wildcards.size >= 1 && continuations.size > 1) {
       throw new Exception(
-        "Routes overlap with wildcards: " +
-          (wildcards ++ continuations).flatMap(_._2).map(_._1.mkString("/"))
+        s"Routes overlap with wildcards: $renderContinuations"
       )
-    }else if (terminals.headOption.exists(_._3) && continuations.size == 1){
+    }
+
+    if (terminals.headOption.exists(_._3) && continuations.size == 1) {
       throw new Exception(
-        "Routes overlap with subpath capture: " +
-          (wildcards ++ continuations).flatMap(_._2).map(_._1.mkString("/"))
-      )
-    }else{
-      DispatchTrie[T](
-        current = terminals.headOption.map(x => x._2 -> x._3),
-        children = continuations.map{ case (k, vs) => (k, construct(index + 1, vs))}.toMap
+        s"Routes overlap with subpath capture: $renderTerminals, $renderContinuations"
       )
     }
   }
+
+  def renderPath(p: collection.Seq[String]) = " /" + p.mkString("/")
 }
 
 /**
@@ -72,4 +111,9 @@ case class DispatchTrie[T](current: Option[(T, Boolean)],
 
     }
   }
+
+  def map[V](f: T => V): DispatchTrie[V] = DispatchTrie(
+    current.map{case (t, v) => (f(t), v)},
+    children.map { case (k, v) => (k, v.map(f))}
+  )
 }

cask/src/cask/main/Main.scala
@@ -154,12 +154,12 @@ object Main{
     }
 
     val dispatchInputs = flattenedRoutes.groupBy(_._1).map { case (segments, values) =>
-      val methodMap = values.map(_._2).flatten.toMap
+      val methodMap = values.map(_._2).flatten
       val hasSubpath = values.map(_._3).contains(true)
       (segments, methodMap, hasSubpath)
     }.toSeq
 
-    DispatchTrie.construct(0, dispatchInputs)
+    DispatchTrie.construct(0, dispatchInputs)(_.map(_._1)).map(_.toMap)
   }
 
   def writeResponse(exchange: HttpServerExchange, response: Response.Raw) = {

example/variableRoutes/app/src/VariableRoutes.scala
@@ -15,5 +15,10 @@ object VariableRoutes extends cask.MainRoutes{
     s"Subpath ${request.remainingPathSegments}"
   }
 
+  @cask.post("/path", subpath = true)
+  def postShowSubpath(request: cask.Request) = {
+    s"POST Subpath ${request.remainingPathSegments}"
+  }
+
   initialize()
 }

cask/test/src/test/cask/DispatchTrieTests.scala
@@ -8,7 +8,7 @@ object DispatchTrieTests extends TestSuite {
     "hello" - {
       val x = DispatchTrie.construct(0,
         Seq((Vector("hello"), 1, false))
-      )
+      )(Seq(_))
 
       assert(
         x.lookup(List("hello"), Map()) == Some((1, Map(), Nil)),
@@ -22,7 +22,7 @@ object DispatchTrieTests extends TestSuite {
           (Vector("hello", "world"), 1, false),
           (Vector("hello", "cow"), 2, false)
         )
-      )
+      )(Seq(_))
       assert(
         x.lookup(List("hello", "world"), Map()) == Some((1, Map(), Nil)),
         x.lookup(List("hello", "cow"), Map()) == Some((2, Map(), Nil)),
@@ -34,7 +34,7 @@ object DispatchTrieTests extends TestSuite {
     "bindings" - {
       val x = DispatchTrie.construct(0,
         Seq((Vector(":hello", ":world"), 1, false))
-      )
+      )(Seq(_))
       assert(
         x.lookup(List("hello", "world"), Map()) == Some((1, Map("hello" -> "hello", "world" -> "world"), Nil)),
         x.lookup(List("world", "hello"), Map()) == Some((1, Map("hello" -> "world", "world" -> "hello"), Nil)),
@@ -47,7 +47,7 @@ object DispatchTrieTests extends TestSuite {
     "path" - {
       val x = DispatchTrie.construct(0,
         Seq((Vector("hello"), 1, true))
-      )
+      )(Seq(_))
 
       assert(
         x.lookup(List("hello", "world"), Map()) ==  Some((1,Map(), Seq("world"))),
@@ -58,44 +58,113 @@ object DispatchTrieTests extends TestSuite {
     }
 
     "errors" - {
-      intercept[Exception]{
+      test - {
         DispatchTrie.construct(0,
           Seq(
             (Vector("hello", ":world"), 1, false),
             (Vector("hello", "world"),  2, false)
           )
+        )(Seq(_))
+
+        val ex = intercept[Exception]{
+          DispatchTrie.construct(0,
+            Seq(
+              (Vector("hello", ":world"), 1, false),
+              (Vector("hello", "world"),  1, false)
+            )
+          )(Seq(_))
+        }
+
+        assert(
+          ex.getMessage ==
+          "Routes overlap with wildcards: 1 /hello/:world, 1 /hello/world"
         )
       }
-      intercept[Exception]{
+      test - {
         DispatchTrie.construct(0,
           Seq(
             (Vector("hello", ":world"), 1, false),
             (Vector("hello", "world", "omg"), 2, false)
           )
+        )(Seq(_))
+
+        val ex = intercept[Exception]{
+          DispatchTrie.construct(0,
+            Seq(
+              (Vector("hello", ":world"), 1, false),
+              (Vector("hello", "world", "omg"), 1, false)
+            )
+          )(Seq(_))
+        }
+
+        assert(
+          ex.getMessage ==
+          "Routes overlap with wildcards: 1 /hello/:world, 1 /hello/world/omg"
         )
       }
-      intercept[Exception]{
+      test - {
         DispatchTrie.construct(0,
           Seq(
             (Vector("hello"), 1, true),
             (Vector("hello", "cow", "omg"), 2, false)
           )
+        )(Seq(_))
+
+        val ex = intercept[Exception]{
+          DispatchTrie.construct(0,
+            Seq(
+              (Vector("hello"), 1, true),
+              (Vector("hello", "cow", "omg"), 1, false)
+            )
+          )(Seq(_))
+        }
+
+        assert(
+          ex.getMessage ==
+          "Routes overlap with subpath capture: 1 /hello, 1 /hello/cow/omg"
         )
       }
-      intercept[Exception]{
+      test - {
         DispatchTrie.construct(0,
           Seq(
             (Vector("hello", ":world"), 1, false),
             (Vector("hello", ":cow"), 2, false)
           )
+        )(Seq(_))
+
+        val ex = intercept[Exception]{
+          DispatchTrie.construct(0,
+            Seq(
+              (Vector("hello", ":world"), 1, false),
+              (Vector("hello", ":cow"), 1, false)
+            )
+          )(Seq(_))
+        }
+
+        assert(
+          ex.getMessage ==
+          "Routes overlap with wildcards: 1 /hello/:world, 1 /hello/:cow"
         )
       }
-      intercept[Exception]{
+      test - {
         DispatchTrie.construct(0,
           Seq(
             (Vector("hello", "world"), 1, false),
             (Vector("hello", "world"), 2, false)
           )
+        )(Seq(_))
+
+        val ex = intercept[Exception]{
+          DispatchTrie.construct(0,
+            Seq(
+              (Vector("hello", "world"), 1, false),
+              (Vector("hello", "world"), 1, false)
+            )
+          )(Seq(_))
+        }
+        assert(
+          ex.getMessage ==
+          "More than one endpoint has the same path: 1 /hello/world, 1 /hello/world"
         )
       }
     }

example/variableRoutes/app/test/src/ExampleTests.scala
@@ -46,6 +46,9 @@ object ExampleTests extends TestSuite{
 
       requests.get(s"$host/path/one/two/three").text() ==>
         "Subpath List(one, two, three)"
+
+      requests.post(s"$host/path/one/two/three").text() ==>
+        "POST Subpath List(one, two, three)"
     }
 
   }