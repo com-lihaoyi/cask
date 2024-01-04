b6b699899119bb21e437ec18bd5b0a9dca49f803
Li Haoyi <haoyi.sg@gmail.com>
2024-01-04 12:58:35 +0800

    .

--- example/variableRoutes/app/test/src/ExampleTests.scala
+++ example/variableRoutes/app/test/src/ExampleTests.scala
@@ -27,68 +27,68 @@ object ExampleTests extends TestSuite{
 
 
       assert(
-        requests.get(s"$host/post/123?param=xyz").text() ==
-          "Post 123 xyz"
+        requests.get(s"$host/article/123?param=xyz").text() ==
+          "Article 123 xyz"
       )
 
-      requests.get(s"$host/post/123", check = false).text() ==>
+      requests.get(s"$host/article/123", check = false).text() ==>
         """Missing argument: (param: String)
           |
           |Arguments provided did not match expected signature:
           |
-          |showPost
-          |  postId  Int
+          |getArticle
+          |  articleId  Int
           |  param  String
           |
           |""".stripMargin
 
       assert(
-        requests.get(s"$host/post2/123?param=xyz").text() ==
-          "Post 123 Some(xyz)"
+        requests.get(s"$host/article2/123?param=xyz").text() ==
+          "Article 123 Some(xyz)"
       )
 
       assert(
-        requests.get(s"$host/post2/123").text() ==
-          "Post 123 None"
+        requests.get(s"$host/article2/123").text() ==
+          "Article 123 None"
       )
 
       assert(
-        requests.get(s"$host/post3/123?param=xyz").text() ==
-          "Post 123 xyz"
+        requests.get(s"$host/article3/123?param=xyz").text() ==
+          "Article 123 xyz"
       )
 
       assert(
-        requests.get(s"$host/post3/123").text() ==
-          "Post 123 DEFAULT VALUE"
+        requests.get(s"$host/article3/123").text() ==
+          "Article 123 DEFAULT VALUE"
       )
 
 
       assert(
-        requests.get(s"$host/post4/123?param=xyz&param=abc").text() ==
-          "Post 123 ArraySeq(xyz, abc)" ||
-        requests.get(s"$host/post4/123?param=xyz&param=abc").text() ==
-          "Post 123 ArrayBuffer(xyz, abc)"
+        requests.get(s"$host/article4/123?param=xyz&param=abc").text() ==
+          "Article 123 ArraySeq(xyz, abc)" ||
+        requests.get(s"$host/article4/123?param=xyz&param=abc").text() ==
+          "Article 123 ArrayBuffer(xyz, abc)"
       )
 
-      requests.get(s"$host/post4/123", check = false).text() ==>
+      requests.get(s"$host/article4/123", check = false).text() ==>
         """Missing argument: (param: Seq[String])
           |
           |Arguments provided did not match expected signature:
           |
-          |showPostSeq
-          |  postId  Int
+          |getArticleSeq
+          |  articleId  Int
           |  param  Seq[String]
           |
           |""".stripMargin
 
       assert(
-        requests.get(s"$host/post5/123?param=xyz&param=abc").text() ==
-          "Post 123 ArraySeq(xyz, abc)" ||
-        requests.get(s"$host/post5/123?param=xyz&param=abc").text() ==
-          "Post 123 ArrayBuffer(xyz, abc)"
+        requests.get(s"$host/article5/123?param=xyz&param=abc").text() ==
+          "Article 123 ArraySeq(xyz, abc)" ||
+        requests.get(s"$host/article5/123?param=xyz&param=abc").text() ==
+          "Article 123 ArrayBuffer(xyz, abc)"
       )
       assert(
-        requests.get(s"$host/post5/123").text() == "Post 123 List()"
+        requests.get(s"$host/article5/123").text() == "Article 123 List()"
       )
 
       requests.get(s"$host/path/one/two/three").text() ==>
