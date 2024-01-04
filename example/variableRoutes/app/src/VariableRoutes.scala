db650abf0a86935a5f5a2bc55650f10a0faab192
Li Haoyi <haoyi.sg@gmail.com>
2024-01-04 13:06:11 +0800

    Flesh out examples for query param handling (#107)

--- example/variableRoutes/app/src/VariableRoutes.scala
+++ example/variableRoutes/app/src/VariableRoutes.scala
@@ -1,22 +1,42 @@
 package app
 object VariableRoutes extends cask.MainRoutes{
   @cask.get("/user/:userName")
-  def showUserProfile(userName: String) = {
+  def getUserProfile(userName: String) = {
     s"User $userName"
   }
 
-  @cask.get("/post/:postId")
-  def showPost(postId: Int, param: Seq[String]) = {
-    s"Post $postId $param"
+  @cask.get("/article/:articleId")
+  def getArticle(articleId: Int, param: String) = { // Mandatory query param
+    s"Article $articleId $param"
+  }
+
+  @cask.get("/article2/:articleId") // Optional query param
+  def getArticleOptional(articleId: Int, param: Option[String] = None) = {
+    s"Article $articleId $param"
+  }
+
+  @cask.get("/article3/:articleId") // Optional query param with default
+  def getArticleDefault(articleId: Int, param: String = "DEFAULT VALUE") = { 
+    s"Article $articleId $param"
+  }
+
+  @cask.get("/article4/:articleId") // 1-or-more query param
+  def getArticleSeq(articleId: Int, param: Seq[String]) = {
+    s"Article $articleId $param"
+  }
+
+  @cask.get("/article5/:articleId") // 0-or-more query param
+  def getArticleOptionalSeq(articleId: Int, param: Seq[String] = Nil) = {
+    s"Article $articleId $param"
   }
 
   @cask.get("/path", subpath = true)
-  def showSubpath(request: cask.Request) = {
+  def getSubpath(request: cask.Request) = {
     s"Subpath ${request.remainingPathSegments}"
   }
 
   @cask.post("/path", subpath = true)
-  def postShowSubpath(request: cask.Request) = {
+  def postArticleSubpath(request: cask.Request) = {
     s"POST Subpath ${request.remainingPathSegments}"
   }
 
