196eb8719167b44d26824a5eae82bcf69bf11f03
He-Pin(kerr) <hepin1989@gmail.com>
2024-05-19 23:46:34 +0800

    chore: add type annotation to caskMetadata

--- cask/src-3/cask/main/Routes.scala
+++ cask/src-3/cask/main/Routes.scala
@@ -7,8 +7,8 @@ import language.experimental.macros
 trait Routes{
 
   def decorators = Seq.empty[cask.router.Decorator[_, _, _]]
-  private[this] var metadata0: RoutesEndpointsMetadata[this.type] = null
-  def caskMetadata =
+  private[this] var metadata0: RoutesEndpointsMetadata[this.type] = _
+  def caskMetadata: RoutesEndpointsMetadata[Routes.this.type] =
     if (metadata0 != null) metadata0
     else throw new Exception("Routes not yet initialized")
 
